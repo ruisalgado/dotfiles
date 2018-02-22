34950146a98fe4df50d3f77dd1ccc0d6333e02e3
Rui Salgado <ruivfsalgado@gmail.com>
2018-02-22 08:44:51 +0000

    ripgrep + fzf

--- zsh/external.zsh
+++ zsh/external.zsh
@@ -63,6 +63,8 @@ fi
 # FZF
 ##
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 ##
 
 ##
