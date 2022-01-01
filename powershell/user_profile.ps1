668e7c5a9dbec8474a9eac0368a5973f8ef86c1b
Rui Salgado <ruivfsalgado@gmail.com>
2022-01-01 10:42:50 +0000

    Windows (powershell) setup

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,38 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+
+# Load prompt config
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'rusal.omp.json'
+oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | iex
+
+# Icons
+Import-Module Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+
+# Alias
+Set-Alias vi nvim
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
